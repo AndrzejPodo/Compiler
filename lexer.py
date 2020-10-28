79cbeaa84854bfc40b8ea8de857d181f0275ba8e
AndrzejPodo <andrzejpodobinski2@gmail.com>
2020-10-28 11:43:46 +0100

    add scanner as starting point for running lexer, add example dir and removed hardcoded examples from lexer

--- lexer.py
+++ lexer.py
@@ -75,49 +75,9 @@ def t_newline(t):
     t.type = ';'
     return t
 
+ # Error handling rule
+def t_error(t):
+    print(f'Illegal character {t.value[0]} at line {t.lineno}' )
+    t.lexer.skip(1)
+
 lexer = lex.lex()
-
-data_1 = '''
-A = zeros(5); # create 5x5 matrix filled with zeros
-B = ones(7);  # create 7x7 matrix filled with ones
-I = eye(10);  # create 10x10 matrix filled with ones on diagonal and zeros elsewhere
-D1 = A.+B' ;  # add element-wise A with transpose of B
-D2 -= A.-B' ; # substract element-wise A with transpose of B
-D3 *= A.*B' ; # multiply element-wise A with transpose of B
-D4 /= A./B' ; # divide element-wise A with transpose of B
-'''
-
-data_2 = '''
-A = zeros(5);  # create 5x5 matrix filled with zeros
-B = ones(7);   # create 7x7 matrix filled with ones
-I = eye(10);   # create 10x10 matrix filled with ones on diagonal and zeros elsewhere
-D1 = A.+B' ; # add element-wise A with transpose of B
-D2 -= A.-B' ; # substract element-wise A with transpose of B
-D3 *= A.*B' ; # multiply element-wise A with transpose of B
-D4 /= A./B' ; # divide element-wise A with transpose of B
-
-E1 = [ [ 1, 2, 3],
-       [ 4, 5, 6],
-       [ 7, 8, 9] ];
-
-res1 = 60.500;
-res2 = 60.;
-res3 = .500;
-res4 = 60.52E2;
-str = "Hello world";
-
-if (m==n) { 
-    if (m >= n) 
-        print res;
-}
-'''
-
-lexer.input(data_2)
-
-# Tokenize
-while True:
-    tok = lexer.token()
-    if not tok: 
-        break   
-    print(f'({tok.lineno}): {tok.type}({tok.value})')
-
